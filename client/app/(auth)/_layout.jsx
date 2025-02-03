a214e811ed2c1b1b0f7164ecb1fb6f68e49a557c
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-02-03 23:06:53 +0545

    wip-index screen

--- client/app/(auth)/_layout.jsx
+++ client/app/(auth)/_layout.jsx
@@ -1,8 +1,14 @@
 import {Stack} from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
 export default function AuthLayout(){
+    const {isLoaded, isSignedIn}= useAuth()
+    if(!isLoaded) return null
+    if(isSignedIn) return <Redirect href={"/index"} />
     return(
         <Stack>
             <Stack.Screen name='index' options={{headerShown:false}} />
+            <Stack.Screen name='sign-up' options={{headerShown:true, title:"Sign Up"}} />
+            <Stack.Screen name='reset-password' options={{headerShown:true, title:"Reset Password"}} />
         </Stack>
     )
 }
--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,7 +1,9 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import { useSignIn } from '@clerk/clerk-expo'
 
 const SignIn = () => {
+  const {signIn, setActive, isLoaded}= useSignIn()
   return (
     <View className='flex-1 items-center justify-center'>
         <SafeAreaView>
