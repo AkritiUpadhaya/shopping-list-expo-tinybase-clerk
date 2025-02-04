48b279db3d8f548114ce065af81290c62a7bb50c
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-02-04 20:26:53 +0545

    modified button component

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,9 +1,14 @@
 import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useSignIn } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
 
 const SignIn = () => {
   const {signIn, setActive, isLoaded}= useSignIn()
+  const router = useRouter();
+  const [email, setEmail]= useState('')
+  const [password, setPassword]= useState("")
+  const [isSignIn, setIsSignIn]= useState(false)
   return (
     <View className='flex-1 items-center justify-center'>
         <SafeAreaView>
