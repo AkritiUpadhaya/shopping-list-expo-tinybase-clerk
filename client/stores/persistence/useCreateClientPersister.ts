02785f4945d30911ee94ffa75abeb5a844bf110d
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-02-11 19:20:51 +0545

    created perister hook

---
+++ client/stores/persistence/useCreateClientPersister.ts
@@ -0,0 +1,27 @@
+import * as UiReact from "tinybase/ui-react/with-schemas"
+import { Content, MergeableStore, OptionalSchemas } from "tinybase/with-schemas";
+import { createClientPersister } from "./createClientPersister";
+
+export const  useCreateClientPersister= <Schemas extends OptionalSchemas>(
+    storeId:string,
+    store: MergeableStore<Schemas>,
+    initialContentJson?: string,
+    then? : ()=> void
+
+)=> 
+(UiReact as UiReact.WithSchemas<Schemas>). useCreatePersister(
+    store,
+    (store: MergeableStore<Schemas>)=> createClientPersister(storeId, store),
+    [storeId],
+    async(persister)=>{
+        let initialContent: Content<Schemas> | undefined= undefined
+        try{
+            initialContent= JSON.parse(initialContentJson)
+        } catch{}
+
+        await persister.load(initialContent)
+        await persister.startAutoSave()
+        then?. ()
+    },
+    [initialContentJson]
+)
